5358c929b39c111889cc22f661d460a31c1f4b42
carlostcba <33902391+carlostcba@users.noreply.github.com>
2024-05-10 07:45:35 -0300

    Create en_cam_rpi.sh

---
+++ en_cam_rpi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Upgrade BIOS
+echo "Upgrading BIOS..."
+sudo rpi-update
+
+# Enable Camera Module
+echo "Enabling Camera Module..."
+sudo raspi-config nonint do_camera 0
+
+# Edit config file
+echo "Editing config file..."
+sudo sed -i '/^start_x=/ s/^/#/' /boot/config.txt
+sudo sed -i '/^camera_auto_detect=/ s/^/#/' /boot/config.txt
+sudo sed -i '/^gpu_mem=/ s/^/#/' /boot/config.txt
+echo "camera_auto_detect=1" | sudo tee -a /boot/config.txt
+echo "gpu_mem=256" | sudo tee -a /boot/config.txt
+
+echo "Configuration complete. Rebooting..."
+sudo reboot
